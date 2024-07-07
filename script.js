269e852d0edd8ed114578c8d3a32c206bc2eb74e
neo-30003 <dr.arrum@gmail.com>
2024-07-07 23:35:12 +0300

    Выполнил 15-й усложнённый урок

--- script.js
+++ script.js
@@ -1,31 +1,58 @@
-const DomElement = function (selector, height, width, bg, fontSize) {
-  this.selector = selector;
-  this.height = height;
-  this.width = width;
-  this.bg = bg;
-  this.fontSize = fontSize;
-};
+document.addEventListener("DOMContentLoaded", function (event) {
+  const px = 10;
+  const DomElement = function (
+    selector = ".default-selector",
+    height = "100px",
+    width = "100px",
+    bg = "green",
+    fontSize = "16px"
+  ) {
+    this.selector = selector;
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
+    this.cssText = "";
+  };
 
-DomElement.prototype.createElem = function () {
-  let element;
-  if (this.selector.startsWith(".")) {
-    element = document.createElement("div");
-    element.classList.add(this.selector);
-    document.body.append(element);
-  } else if (this.selector.startsWith("#")) {
-    element = document.createElement("p");
-    element.setAttribute("id", this.selector);
-    document.body.append(element);
-  }
-  element.style.cssText = `height: ${this.height}px;
-  width: ${this.width}px;
-  background: ${this.bg};
-  font-size: ${this.fontSize}px;`;
-  element.textContent = "Какой-нибудь текст";
-};
+  DomElement.prototype.newElem = function () {
+    let elem;
+    elem = document.createElement("div");
+    if (this.selector[0] === ".") {
+      elem = document.createElement("div");
+      elem.className = this.selector.slice(1);
+    }
+    elem.style.cssText = `height: ${this.height};
+    width: ${this.width};
+    background: ${this.bg};
+    font-size: ${this.fontSize};`;
 
-const newElem1 = new DomElement(".block", "100", "200", "green", "25");
-const newElem2 = new DomElement("#best", "100", "200", "red", "25");
+    elem.style.cssText += `left: ${Math.round(
+      window.innerWidth / 2 - parseInt(this.width) / 2
+    )}px; `;
+    elem.style.cssText += `top: ${Math.round(
+      window.innerHeight / 2 - parseInt(this.height) / 2
+    )}px; `;
+    elem.style.cssText += `position: absolute;`;
+    elem.addEventListener("click", this.transfer);
+    return elem;
+  };
 
-newElem1.createElem();
-newElem2.createElem();
+  const square = new DomElement();
+  document.body.appendChild(square.newElem());
+  document.addEventListener("keydown", (event) => {
+    const div = document.querySelector("div");
+    if (event.key === "ArrowUp") {
+      div.style.top = parseInt(div.style.top) - px + "px";
+    }
+    if (event.key === "ArrowRight") {
+      div.style.left = parseInt(div.style.left) + px + "px";
+    }
+    if (event.key === "ArrowLeft") {
+      div.style.left = parseInt(div.style.left) - px + "px";
+    }
+    if (event.key === "ArrowDown") {
+      div.style.top = parseInt(div.style.top) + px + "px";
+    }
+  });
+});
